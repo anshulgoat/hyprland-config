867a10a2f875c1087ed9ac263619837d7e43b6e0
anshulgoat <anshulgoat@gmail.com>
2026-02-12 01:28:54 +0530

    initial commit

---
+++ cmus/lib.pl
@@ -0,0 +1,4 @@
+/home/anshul/Music/[2024] Super Real Me/ILLIT - Super Real Me - 01 - My World.flac
+/home/anshul/Music/[2024] Super Real Me/ILLIT - Super Real Me - 02 - Magnetic.flac
+/home/anshul/Music/[2024] Super Real Me/ILLIT - Super Real Me - 03 - Midnight Fiction.flac
+/home/anshul/Music/[2024] Super Real Me/ILLIT - Super Real Me - 04 - Lucky Girl Syndrome.flac
